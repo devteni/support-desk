58f642b1116a960fff4a2deb64698afa39452977
devteni <fatunmbiteniola001@gmail.com>
2022-12-28 20:34:37 +0100

    :sparkles: implemented ticket closing functionality

--- src/components/TicketItem.tsx
+++ src/components/TicketItem.tsx
@@ -13,9 +13,9 @@ export const TICKET_STATUS: Record<string, string> = {
 const TicketItem: React.FC<Props> = ({ticket }) => {
   return (
     <tr className='hover:bg-gray-100'>
-        <td className='px-10 py-4'>{new Date(ticket.createdAt).toLocaleString('en-US')}</td>
-        <td className='px-10 py-4'>{ticket.product}</td>
-        <td className={`px-10 py-4`}><span className={`px-4 py-2 rounded-lg ${TICKET_STATUS[`${ticket.status}`]}`}>{ticket.status}</span></td>
+        <td className='px-10 py-4 text-center'>{new Date(ticket.createdAt).toLocaleString('en-US')}</td>
+        <td className='px-10 py-4 text-center'>{ticket.product}</td>
+        <td className={`px-10 py-4 text-center`}><span className={`px-10 py-2 rounded-lg ${TICKET_STATUS[`${ticket.status}`]}`}>{ticket.status}</span></td>
 
         <td className='px-10 py-4'><Link to={`/tickets/${ticket._id}`} className="hover:underline">View</Link></td>
     </tr>
